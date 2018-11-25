dc7ec771c21edd3c410f0f2f333b5613cccccaef
TsArabadzhiyska <c.arabadzhiyska@abv.bg>
2018-11-26 00:40:02 +0200

    Initial commit

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /<controller>/
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = ID;
+            return View();
+        }
+
+        public string HelloToYou(string name ,int age=4)
+        {
+            return HtmlEncoder.Default.Encode($"Hey {name} hello to you . You are {age} years old");
+        }
+    }
+}
